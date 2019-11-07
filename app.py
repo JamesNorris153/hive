7fd6276398e776afb3120bdb934950d32faa929b
James Norris <James.Norris153@gmail.com>
2019-11-07 16:28:48 +0000

    Added block class

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #app.py
+from models.block import block
 
 def run():
 	print("suck on my balls")
+	block1 = block([1, 2], 1, 1)
+	print(block1.compute_hash())
 
 run()
--- models/block.py
+++ models/block.py
@@ -1,5 +1,12 @@
-class block():
+import json
+from hashlib import sha256
+
+class block:
 	def __init__(self, index, transactions, timestamp):
 		self.index = []
 		self.transactions = transactions
 		self.timestamp = timestamp
+
+	def compute_hash(self):
+		block_string = json.dumps(self.__dict__, sort_keys = True)
+		return sha256(block_string.encode()).hexdigest()
