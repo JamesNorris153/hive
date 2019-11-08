3233568ae45d9f55f47f058d5d840ec3b7e18e09
James Norris <James.Norris153@gmail.com>
2019-11-08 14:50:27 +0000

    Preparing for merge

--- app.py
+++ app.py
@@ -4,16 +4,7 @@ from models.blockchain import blockchain
 def run():
 	hive = blockchain()
 
-	hive.add_new_transaction("Hello")
-	hive.add_new_transaction("Wassup")
-	hive.add_new_transaction("Bye")
-	hive.add_new_transaction("Mmmmk bye")
-
-	hive.mine()
-
-	hive.add_new_transaction("awesome")
-	hive.add_new_transaction("sick")
-
+	hive.add_new_transaction("Hello World!")
 	hive.mine()
 
 	for honeycomb in hive.chain:
